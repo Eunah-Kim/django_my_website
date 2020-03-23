497b09fa74784d35f7d328ee24830f4e71571eb5
Eunah <keunah1016@naver.com>
2020-03-23 14:44:31 +0900

    post create page

--- blog/urls.py
+++ blog/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('category/<str:slug>/', views.PostListByCategory.as_view()),
     path('tag/<str:slug>/', views.PostListByTag.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('create/', views.PostCreate.as_view()),
     path('', views.PostList.as_view()),
 ]
