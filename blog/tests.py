5eeaa1e05692d2e0153739cb5178308623980858
Eunah <keunah1016@naver.com>
2020-03-23 11:52:13 +0900

    post_detail_page_with_tag_tested

--- blog/tests.py
+++ blog/tests.py
@@ -124,11 +124,15 @@ class TestView(TestCase):
 
 
     def test_post_list_with_post(self):
+        tag_america = create_tag(name='america')
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world.',
             author=self.author_000,
         )
+        post_000.tags.add(tag_america)
+        post_000.save()
+
 
         post_001 = create_post(
             title='The second post',
@@ -136,6 +140,8 @@ class TestView(TestCase):
             author=self.author_000,
             category=create_category(name='정치/사회')
         )
+        post_001.tags.add(tag_america)
+        post_001.save()
 
         self.assertGreater(Post.objects.count(), 0)
 
@@ -156,13 +162,20 @@ class TestView(TestCase):
         self.assertIn('정치/사회', main_div.text)  ###  '정치/사회' 있어야 함
         self.assertIn('미분류', main_div.text)  ### '미분류' 있어야 함
 
+        # Tag
+        post_card_000 = main_div.find('div', id='post-card-{}'.format(post_000.pk))
+        self.assertIn('#america', post_card_000.text) # Tag가 해당 post의 card마다 있다.
+
 
     def test_post_detail(self):
+        tag_america = create_tag(name='america')
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world.',
             author=self.author_000,
         )
+        post_000.tags.add(tag_america)
+        post_000.save()
 
         post_001 = create_post(
             title='The second post',
@@ -170,6 +183,8 @@ class TestView(TestCase):
             author=self.author_000,
             category=create_category(name='정치/사회')
         )
+        post_001.tags.add(tag_america)
+        post_001.save()
 
         self.assertGreater(Post.objects.count(), 0)
         post_000_url = post_000.get_absolute_url()
@@ -193,6 +208,9 @@ class TestView(TestCase):
         self.assertIn(post_000.content, main_div.text)
 
         self.check_right_side(soup)
+
+        # Tag
+        self.assertIn('#america', main_div.text)  # Tag가 해당 post의 card마다 있다.
 
 
     def test_post_list_no_category(self):
